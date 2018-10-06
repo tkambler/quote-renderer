34c422c757a9babc33b82bb81d52fb7ddb518e03
Tim Ambler <tkambler@gmail.com>
2018-10-06 16:52:37 -0500

    misc

--- run.js
+++ run.js
@@ -1,10 +1,9 @@
 const Renderer = require('./lib/renderer');
 const path = require('path');
-const fs = require('fs');
 
 const renderer = new Renderer({
     'font_path': path.resolve(__dirname, 'assets/fonts/american-typewriter'),
-    'quotes': fs.readFileSync('./quotes.txt', 'utf8')
+    'quotes': path.resolve(__dirname, 'quotes.txt')
 });
 
 renderer.run();
