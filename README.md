323a8dd6f12f60c5dec734bdf4f2d902f35e0f92
Tim Ambler <tkambler@gmail.com>
2018-10-06 18:41:55 -0500

    misc

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Quote Renderer
 
 ---
+
+
+
+## Related Resources
+
+- [Littera](http://kvazars.com/littera/) - A web-based application (requires Flash) for converting commonly-used font formats to bitmap fonts.
--- run.js
+++ run.js
@@ -2,7 +2,15 @@ const Renderer = require('./lib/renderer');
 const path = require('path');
 
 const renderer = new Renderer({
+    // The folder from which fonts are to be loaded
     'font_path': path.resolve(__dirname, 'assets/fonts/american-typewriter'),
+    // The folder to which generated images are to be saved
+    'output_path': path.resolve(__dirname, 'output'),
+    // The folder from which to pull background images.
+    'background_path': path.resolve(__dirname, 'assets/images'),
+    // A text file containing quotes. Each quote should appear on its own line. The quote and the source
+    // should be separated by two dashes (--). For example:
+    // Brevity is the soul of wit. -- William Shakespeare
     'quotes': path.resolve(__dirname, 'quotes.txt')
 });
 
